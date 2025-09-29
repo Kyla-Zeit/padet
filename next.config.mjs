0166a0c92357ada19d46542f500361c5e3577dde
Kyla <112965597+Kyla-Zeit@users.noreply.github.com>
2025-09-29 03:39:31 +0000

    Export static site into /docs for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+const repo = 'padet'
 
-export default nextConfig
+export default {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+}
